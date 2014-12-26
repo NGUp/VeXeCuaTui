93fdf504a7adb37eb949ff64e931cc4ffefa535b
namvh <vhnam2504@gmail.com>
2014-12-26 10:55:58 +0700

    fix Currency format and effect transition.

--- Source/public/js/express/search-result.js
+++ Source/public/js/express/search-result.js
@@ -8,6 +8,11 @@
 
             var type = $($($(this).parents()[1]).children()[4]).html().trim();
 
+            Number.prototype.format = function(n, x) {
+                var re = '(\\d)(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+                return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$1,');
+            };
+
             $.ajax({
                 type: "POST",
                 url: "/syn/gettripinfo",
@@ -23,7 +28,7 @@
                     $("#result-date").html(data[0].NgayDi);
                     $("#result-from").html(data[0].NoiDi);
                     $("#result-to").html(data[0].NoiDen);
-                    $("#result-price").html(data[0].Gia + " VND");
+                    $("#result-price").html(data[0].Gia.format() + " VND");
                     $("#result-logo").attr("src", "/img/Operators/" + data[0].Logo);
 
                     if (type == "Giường Nằm") {
@@ -33,6 +38,10 @@
                         $("#book-bed").hide();
                         $("#book-seat").show();
                     }
+
+                    $("#book-step-1").removeClass("badge search-step-active");
+                    $("#book-step-2").addClass("badge search-step-active");
+                    $("#book-step-3").removeClass("badge search-step-active");
 
                     $("#step-2").fadeIn("slow");
                     $("#step-1").fadeOut("slow");
