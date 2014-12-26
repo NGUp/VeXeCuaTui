da9846c8fbce9fb95b9c32db9c91317391aed65d
namvh <vhnam2504@gmail.com>
2014-12-26 17:54:48 +0700

    fix Edit Chosen Seats.

--- Source/public/js/express/book-ticket.js
+++ Source/public/js/express/book-ticket.js
@@ -2,6 +2,8 @@
     var app = angular.module("book-ticket", []);
 
     app.controller("BookTicketController", function($scope) {
+        var seats = [];
+
         $scope.back = function() {
             $("#result-operator").empty();
             $("#result-date").empty();
@@ -9,13 +11,26 @@
             $("#result-to").empty();
             $("#result-price").empty();
 
+            seats = [];
+
             $("#step-2").fadeOut();
             $("#step-1").fadeIn();
         };
 
         $(".book-seat td").click(function() {
-            var seat = $(this);
-            console.log(seat);
+            var seat = $(this).context;
+
+            if (seat.className == "seat-available") {
+                $(seat).removeClass();
+                $(seat).addClass("seat-checked");
+                seats.push(seat.innerHTML);
+            } else if (seat.className == "seat-checked") {
+                $(seat).removeClass();
+                $(seat).addClass("seat-available");
+                seats.splice(seats.indexOf(seat.innerHTML), 1);
+            }
+
+            console.log(seats);
         })
     })
 })();
